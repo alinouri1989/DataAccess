a00a9b246a381ffe25821cadf38c67b05823a13a
arghiani <arghiani@E-ARGHIYANI-PC>
2025-10-26 16:51:10 +0330

    fix Excel Export

--- Common/Export/ExcelService.cs
+++ Common/Export/ExcelService.cs
@@ -11,7 +11,7 @@ namespace Common.Export
 {
     public class ExcelService : IExcelService
     {
-        public async Task<byte[]> Write<T>(IList<T> registers)
+        public async Task<byte[]> Write<T>(IList<T> registers, bool rightToLeft = false, bool addRowNumber = false)
         {
             var registersTotalRows = registers.Count;
 
@@ -19,8 +19,27 @@ namespace Common.Export
             {
                 var excelWorksheet = excelPackage.Workbook.Worksheets.Add("خروجی");
 
+                if (rightToLeft)
+                {
+                    excelWorksheet.View.RightToLeft = true;
+                }
+
                 Type type = typeof(T);
                 PropertyInfo[] properties = type.GetProperties();
+
+                int totalColumns = properties.Length;
+                if (addRowNumber)
+                {
+                    totalColumns += 1;
+                }
+
+                int colIndex = 1;
+
+                if (addRowNumber)
+                {
+                    excelWorksheet.Cells[1, colIndex].Value = "ردیف";
+                    colIndex++;
+                }
 
                 for (var i = 0; i < properties.Length; i++)
                 {
@@ -32,54 +51,73 @@ namespace Common.Export
                         value = (attribute as DisplayAttribute).Name;
                     }
 
-                    excelWorksheet.Cells[1, i + 1].Value = value;
+                    excelWorksheet.Cells[1, colIndex].Value = value;
+                    colIndex++;
                 }
 
                 int index = 0;
                 for (int row = 2; row <= registersTotalRows + 1; row++)
                 {
+                    colIndex = 1;
+
+                    if (addRowNumber)
+                    {
+                        excelWorksheet.Cells[row, colIndex].Value = index + 1;
+                        if (rightToLeft)
+                        {
+                            excelWorksheet.Cells[row, colIndex].Style.HorizontalAlignment = ExcelHorizontalAlignment.Right;
+                        }
+                        colIndex++;
+                    }
+
                     for (var i = 0; i < properties.Length; i++)
                     {
                         var value = properties[i].GetValue(registers[index], null);
-
                         Type propertyType = properties[i].PropertyType;
                         TypeCode typeCode = Type.GetTypeCode(propertyType);
-
-                        var columnIndex = i + 1;
 
                         switch (typeCode)
                         {
                             case TypeCode.String:
-                                excelWorksheet.Cells[row, columnIndex].Value = value;
+                                excelWorksheet.Cells[row, colIndex].Value = value;
                                 break;
 
                             case TypeCode.Int32:
                             case TypeCode.Double:
                             case TypeCode.Decimal:
                             case TypeCode.Single:
-                                excelWorksheet.Cells[row, columnIndex].Value = value?.ToString();
+                                excelWorksheet.Cells[row, colIndex].Value = value?.ToString();
                                 break;
 
                             case TypeCode.Boolean:
-                                excelWorksheet.Cells[row, columnIndex].Value = (bool)value ? "Yes" : "No";
+                                excelWorksheet.Cells[row, colIndex].Value = (bool)value ? "Yes" : "No";
                                 break;
 
                             case TypeCode.DateTime:
-                                excelWorksheet.Cells[row, columnIndex].Value = ((DateTime)value).ToString("dd/MM/yyyy HH:mm:ss");
+                                excelWorksheet.Cells[row, colIndex].Value = ((DateTime)value).ToString("dd/MM/yyyy HH:mm:ss");
                                 break;
 
                             default:
-                                excelWorksheet.Cells[row, columnIndex].Value = value?.ToString();
+                                excelWorksheet.Cells[row, colIndex].Value = value?.ToString();
                                 break;
                         }
-                    }
 
+                        if (rightToLeft)
+                        {
+                            excelWorksheet.Cells[row, colIndex].Style.HorizontalAlignment = ExcelHorizontalAlignment.Right;
+                        }
+                        colIndex++;
+                    }
                     index++;
                 }
+
                 excelWorksheet.Cells.AutoFitColumns();
 
-                var excelTable = excelWorksheet.Tables.Add(new ExcelAddressBase(fromRow: 1, fromCol: 1, toRow: registersTotalRows, toColumn: properties.Length), "TestRegisters");
-                //excelTable.ShowHeader = true;
+
+                var excelTable = excelWorksheet.Tables.Add(
+                    new ExcelAddressBase(fromRow: 1, fromCol: 1, toRow: registersTotalRows + 1, toColumn: totalColumns),
+                    "TestRegisters"
+                );
 
                 var excelRange = excelWorksheet.Cells[1, 1, excelWorksheet.Dimension.End.Row, excelWorksheet.Dimension.End.Column];
                 excelRange.Style.Border.BorderAround(ExcelBorderStyle.Thin);
@@ -92,6 +130,6 @@ namespace Common.Export
     }
     public interface IExcelService
     {
-        Task<byte[]> Write<T>(IList<T> registers);
+        Task<byte[]> Write<T>(IList<T> registers, bool rightToLeft = false, bool addRowNumber = false);
     }
 }
